5d0ed88c1b0ba1d1bbaf27ed3ba873678547c43f
James Brunner <james@brunnertechnicalservices.com>
2019-06-05 20:46:48 -0600

    Makes the initial setup for CI/CD on AWS CodePipeline

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const targetAddress = new URL(process.env.TARGET_ADDRESS || `http://pimtector.local`);
+
 module.exports = {
-  /* Your site config here */
+	resolve: 'gatsby-plugin-s3',
+	options: {
+		bucketName: process.env.TARGET_BUCKET_NAME || 'fake-bucket',
+		region: process.env.AWS_REGION,
+		protocol: targetAddress.protocol.slice(0, -1),
+		hostname: targetAddress.hostname,
+		acl: null,
+		params: {
+			// add custom content types here see:
+			// https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
+		}
+	},
+	resolve: 'gatsby-plugin-canonical-urls',
+	options: {
+		siteUrl: targetAddress.href.slice(0, -1)
+	}
 }
