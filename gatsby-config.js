998bf744c85844ad272c9a7511dbecf4dd5e4409
James Brunner <james@brunnertechnicalservices.com>
2019-06-05 21:28:57 -0600

    Fix broken exports

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,20 +7,26 @@
 const targetAddress = new URL(process.env.TARGET_ADDRESS || `http://pimtector.local`);
 
 module.exports = {
-	resolve: 'gatsby-plugin-s3',
-	options: {
-		bucketName: process.env.TARGET_BUCKET_NAME || 'fake-bucket',
-		region: process.env.AWS_REGION,
-		protocol: targetAddress.protocol.slice(0, -1),
-		hostname: targetAddress.hostname,
-		acl: null,
-		params: {
-			// add custom content types here see:
-			// https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-s3',
+			options: {
+				bucketName: process.env.TARGET_BUCKET_NAME || 'fake-bucket',
+				region: process.env.AWS_REGION,
+				protocol: targetAddress.protocol.slice(0, -1),
+				hostname: targetAddress.hostname,
+				acl: null,
+				params: {
+					// add custom content types here see:
+					// https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
+				}
+			}
+		},
+		{
+			resolve: 'gatsby-plugin-canonical-urls',
+			options: {
+				siteUrl: targetAddress.href.slice(0, -1)
+			}
 		}
-	},
-	resolve: 'gatsby-plugin-canonical-urls',
-	options: {
-		siteUrl: targetAddress.href.slice(0, -1)
-	}
+	]
 }
