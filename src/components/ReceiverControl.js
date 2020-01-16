7a18743680085d2ebf1f4c83f16a1d04d2b8d74e
James Brunner <james@brunnertechnicalservices.com>
2020-01-16 14:31:37 -0700

    Add some more indicators to the receiver control panel

--- src/components/ReceiverControl.js
+++ src/components/ReceiverControl.js
@@ -4,21 +4,27 @@ import PropTypes from 'prop-types';
 const ReceiverControl = ({ Fstart_MHz, Fstop_MHz, RBW_kHz, settings }) => {
 
 	const span_kHz = 1000 * (Fstop_MHz - Fstart_MHz);
+	const Fo_MHz = Fstart_MHz + (span_kHz / 2 * 1000);
 
 	return (
 		<div className='receiver' id='receiver-1'>
 			<div className='row'>
-				<div>
+				<div className='ind'>
 					<span className='label'>Start</span><span className='value'>{Fstart_MHz.toFixed(3)}</span><span className='unit'>MHz</span>
 				</div>
-				<div>
+				<div className='ind'>
+					<span className='label'>Stop</span><span className='value'>{Fstop_MHz.toFixed(3)}</span><span className='unit'>MHz</span>
+				</div>
+			</div>
+			<div className='row'>
+				<div className='ind'>
+					<span className='label'>Center</span><span className='value'>{Fo_MHz.toFixed(3)}</span><span className='unit'>MHz</span>
+				</div>
+				<div className='ind'>
 					<span className='label'>Span</span><span className='value'>{span_kHz.toFixed(3)}</span><span className='unit'>kHz</span>
 				</div>
-				<div>
+				<div className='ind'>
 					<span className='label'>RBW</span><span className='value'>{RBW_kHz.toFixed(3)}</span><span className='unit'>kHz</span>
-				</div>
-				<div>
-					<span className='label'>Stop</span><span className='value'>{Fstop_MHz.toFixed(3)}</span><span className='unit'>MHz</span>
 				</div>
 			</div>
 		</div>
