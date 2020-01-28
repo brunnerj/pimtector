0239b3f306c3ab5f46be0ad869701020a9245dd5
James Brunner <james@brunnertechnicalservices.com>
2020-01-28 09:52:45 -0700

    Add a background video and style accordingly

--- src/components/ConnectionPanel.js
+++ src/components/ConnectionPanel.js
@@ -7,10 +7,23 @@ import Showcase from '../images/showcase-01.png';
 import LeftWand from '../images/wand-left.png';
 import RightWand from '../images/wand-right.png';
 
+import BgVideo from "../assets/Abstract - 26011.mp4";
+
 const ConnectionPanel = ({ panel, panelTimeout, connect, isConnecting }) => {
 
 	return (
 		<Panel id={PANELS.CONNECTION} hideTitle={true} panel={panel} panelTimeout={panelTimeout}>
+
+			{/*
+			<div className='bg'>
+				<img alt='' src={Showcase} />
+				<img className='wand-left' alt='' src={LeftWand} />
+				<img className='wand-right' alt='' src={RightWand} />
+			</div>-->*/}
+
+			<video poster="" id="bgvid" playsInline autoPlay muted loop>
+				<source src={BgVideo} type="video/mp4"/>
+`			</video>
 
 			<div className='cta'>
 				<div className='lead-in'>Introducing PIM<span>tector</span>&trade;</div>
@@ -30,12 +43,6 @@ const ConnectionPanel = ({ panel, panelTimeout, connect, isConnecting }) => {
 
 			</div>
 			
-			<div className='bg'>
-				<img alt='' src={Showcase} />
-				<img className='wand-left' alt='' src={LeftWand} />
-				<img className='wand-right' alt='' src={RightWand} />
-			</div>
-
 			<Spinner showing={isConnecting} label={'Connecting...'} />
 		</Panel>
 	);
